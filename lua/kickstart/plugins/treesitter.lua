88ffa0712d12a12131ddd502e8bb0fb2e4a26d2e
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2025-03-01 09:42:20 -1000

    reconfig treesiter.lua

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -5,8 +5,28 @@ return {
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
+      ensure_installed = {
+        'c',
+        'cpp',
+        'go',
+        'lua',
+        'python',
+        'rust',
+        'tsx',
+        'javascript',
+        'typescript',
+        'vimdoc',
+        'vim',
+        'yaml',
+        'bash',
+        'awk',
+        'json',
+        'latex',
+        'markdown',
+        'markdown_inline',
+        'html',
+      },
       auto_install = true,
       highlight = {
         enable = true,
